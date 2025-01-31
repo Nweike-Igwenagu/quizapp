a5d33deee6cda92732167783bdc015d144769b39
Chinweike <chinwksigwenagu@gmail.com>
2025-01-31 04:09:00 +0100

    made the probability to previously returned questions low

--- api/views.py
+++ api/views.py
@@ -6,15 +6,27 @@ from .serializers import QuestionSerializer
 from rest_framework.exceptions import NotFound
 from django.db.models.functions import Random
 
-
-# Create your views here.
-
 class QuestionListView(APIView):
     def get(self, request, category, pagination):
+        # Get the list of previously returned question IDs from the session (if any)
+        previous_questions = request.session.get('previous_questions', [])
+
+        # Fetch random questions, excluding those previously returned
         try:
-            queryset = Question.objects.filter(Category__name=category).order_by(Random())[:pagination]
+            queryset = Question.objects.filter(
+                Category__name=category
+            ).exclude(id__in=previous_questions).order_by(Random())[:pagination]
+
+            if not queryset:
+                raise NotFound("Questions not found.")
+
         except Question.DoesNotExist:
             raise NotFound("Questions not found.")
 
+        # Update the session with the new list of returned question IDs
+        new_question_ids = [question.id for question in queryset]
+        request.session['previous_questions'] = previous_questions + new_question_ids
+
+        # Serialize the questions and return the response
         serializer = QuestionSerializer(queryset, many=True)
         return Response(serializer.data)
